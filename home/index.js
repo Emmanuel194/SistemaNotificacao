499ab632b5c11d0dd5646886ad9d02e70afc5c59
emmanuel.lima <86535005+Emmanuel194@users.noreply.github.com>
2023-08-30 22:24:50 -0300

    commit atualizações

---
+++ home/index.js
@@ -0,0 +1,12 @@
+const email = document.querySelector(".user");
+const password = document.querySelector(".password");
+const button = document.querySelector(".button");
+
+
+button.addEventListener("click", enviando)  // event list de click
+
+function enviando () {  // function de requisição para quando cliar no button enviar.
+  alert ("ola")
+}
+
+
